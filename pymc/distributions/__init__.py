51098925a461c6e3a208058e3c96445e7f690efb
Max Kochurov <maxim.v.kochurov@gmail.com>
2023-12-15 12:42:45 +0000

    rework KL

--- pymc/distributions/__init__.py
+++ pymc/distributions/__init__.py
@@ -114,6 +114,12 @@ from pymc.distributions.timeseries import (
 )
 from pymc.distributions.truncated import Truncated
 
+# no dispatched stats are being initialized
+# isort: off
+import pymc.distributions._stats
+
+# isort: on
+
 __all__ = [
     "Uniform",
     "Flat",
